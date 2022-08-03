8cc346ecd65ea1346ffff989618a4284259b8599
Kamyar Mirzavaziri <kmirzavaziri@gmail.com>
2022-08-03 09:35:21 +0430

    use textarea for contact us text

--- pages/forms.py
+++ pages/forms.py
@@ -7,4 +7,4 @@ from main.forms import BootstrapForm
 class ContactUsForm(BootstrapForm):
     title = forms.CharField(label=exp('contact_us.title'), min_length=1, max_length=50)
     email = forms.EmailField(label=exp('contact_us.email'), widget=forms.EmailInput(attrs={'class': 'force-ltr'}))
-    text = forms.CharField(label=exp('contact_us.text'), min_length=10, max_length=250)
+    text = forms.CharField(label=exp('contact_us.text'), min_length=10, max_length=250, widget=forms.Textarea())
