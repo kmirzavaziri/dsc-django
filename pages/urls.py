8ce4e1ac4178b85c46f86fb0f7152a75ff1e8227
Kamyar Mirzavaziri <kmirzavaziri@gmail.com>
2022-08-03 11:01:48 +0430

    minor cleanings and improvements

--- pages/urls.py
+++ pages/urls.py
@@ -5,6 +5,6 @@ from .views import home_view, contact_us_view, contact_us_success_view
 app_name = 'pages'
 urlpatterns = [
     path('', home_view, name='home'),
-    path('contact-us', contact_us_view, name='contact_us'),
-    path('contact-us-success', contact_us_success_view, name='contact_us_success'),
+    path('contact-us/', contact_us_view, name='contact_us'),
+    path('contact-us-success/', contact_us_success_view, name='contact_us_success'),
 ]
