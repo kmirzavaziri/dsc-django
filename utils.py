5b52d5911bf6d2fdfcfc57a592b7cb02c623cc9a
Kamyar Mirzavaziri <kmirzavaziri@gmail.com>
2022-08-03 12:26:31 +0430

    wrapped send email in a try block

--- utils.py
+++ utils.py
@@ -2,4 +2,8 @@ from django.core.mail import send_mail
 
 
 def send_email(*, subject, body, to_email, from_email):
-    send_mail(subject=subject, message=body, from_email=from_email, recipient_list=[to_email], fail_silently=True)
+    try:
+        send_mail(subject=subject, message=body, from_email=from_email, recipient_list=[to_email], fail_silently=False)
+    except Exception as e:
+        # @TODO log e
+        pass
